29c0752b17c34faec92fa90f63afa82157c73ddb
Aman Chaudhary <aman.chaudhary214@gmail.com>
2023-08-22 21:46:01 +0530

    code added

---
+++ hello-world/src/App.vue
@@ -0,0 +1,29 @@
+<template> <div id="app"> <h1>{{ message }}</h1> </div> </template>
+
+
+<script> 
+export default { 
+  data() { 
+    return { 
+      message: 'Hello, World! \n Welcome to my first Vue app.' 
+    } 
+  },
+  methods: {
+    formattedmessage() {
+      return this.message.replace(/\n/gi, '<br/>');
+    }
+  }
+} 
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
